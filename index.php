e387c2e4a9148c6c201b5b1744b503db0f5d4d41
Rafael Bota <rafaelcosta.meiabota@gmail.com>
2020-06-01 09:41:41 +0100

    clean branch

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pathOfThePageWhereTheRequestWasMade=$_SERVER["HTTP_REFERER"];
-$pathOfThePageWhereTheRequestWasMade=explode("/View",$pathOfThePageWhereTheRequestWasMade);
-$pathToListenerPodcast=$pathOfThePageWhereTheRequestWasMade[0]."/View/index.php";
-header("Location:".$pathToListenerPodcast);
-?>
